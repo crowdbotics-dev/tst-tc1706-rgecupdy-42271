235f82baa1c06ddc0b74e6097cfe7ea1e367c2d3
Firsttwvqmkxria Lastoicfasaznl <automation2@crowdbotics.com>
2022-11-02 05:29:40 +0000

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={_styles.qHRvfBmG}>
+      </View>;
+};
+
+export default Untitled2;
+
+const _styles = StyleSheet.create({
+  qHRvfBmG: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
